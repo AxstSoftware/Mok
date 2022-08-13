0bf360ca0056057cb9cd087d3a85aff06700154e
xNtrow <xntrowdev@gmail.com>
2022-08-13 18:12:06 +0200

    Updates [+] Smooth rendering

--- src/main/java/io/github/axst/api/render/UIButton.java
+++ src/main/java/io/github/axst/api/render/UIButton.java
@@ -5,8 +5,12 @@ import io.github.axst.util.RenderUtilities;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 
 import java.awt.*;
+
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
 
 public class UIButton extends GuiButton {
 
@@ -20,10 +24,14 @@ public class UIButton extends GuiButton {
                 mouseY >= this.xPosition &&
                 mouseX < this.xPosition + this.width &&
                 mouseY < this.yPosition + this.height;
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_POINT_SMOOTH);
+        GL11.glEnable(GL13.GL_MULTISAMPLE);
         RenderUtilities.drawRoundedRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + this.height, 5, new Color(164, 172, 180, 64).getRGB());
         RenderUtilities.drawRoundedOutline(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + this.height, 5, 2, new Color(164, 172, 180, 255).getRGB());
-        GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         Mok.getInstance().font.drawString(this.displayString, this.xPosition + ((this.width - mc.fontRendererObj.getStringWidth(this.displayString)) >> 1), this.yPosition + ((this.height - 8) >> 1), new Color(255, 255, 255).getRGB());
     }
 }
