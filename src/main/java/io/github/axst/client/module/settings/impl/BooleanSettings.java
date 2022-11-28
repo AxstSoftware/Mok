dcc64bc7d21e8c915bdff66eab4b7d6115d4c29e
sdxqw <xntrowdev@gmail.com>
2022-11-28 21:10:20 +0100

    Dang this src is clean

--- src/main/java/io/github/axst/client/module/settings/impl/BooleanSettings.java
+++ src/main/java/io/github/axst/client/module/settings/impl/BooleanSettings.java
@@ -4,10 +4,10 @@ import io.github.axst.client.module.settings.Settings;
 import lombok.Getter;
 import lombok.Setter;
 
+@Setter
+@Getter
 public class BooleanSettings extends Settings {
 
-    @Setter
-    @Getter
     public boolean enabled;
 
     public BooleanSettings(String name, boolean enable) {
